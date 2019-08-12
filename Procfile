a7d25f11f3cce73ea1bf6eeb09f7a475ca70d737
Austin Blaine <theaustinblaine@gmail.com>
2019-08-12 13:38:31 -0400

    attempting to deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn indiepk_project.wsgi --log-file -
--- indiepk_project/settings.py
+++ indiepk_project/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions',
+    'indiepk_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,11 @@ WSGI_APPLICATION = 'indiepk_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'indiepk_database',
+        'USER': 'theaustinblaine',
+        'PASSWORD': 'macbeth1',
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static')
+]
+
+django_heroku.settings(locals())

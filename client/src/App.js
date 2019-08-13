0d7c1739ba5c7e163ba1f460a73a0f2b077c988f
Austin Blaine <theaustinblaine@gmail.com>
2019-08-13 11:02:06 -0400

    Beginning buildout of front end.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import ArtistList from "./components/ArtistList";
+import Artist from "./components/Artist";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+
+                    <div>
+                        <h1>indiEPK</h1>
+                        <div>
+                            <div><Link to="/">All Artists</Link></div>
+                        </div>
+                    </div>
+
+                    <Switch>
+                      <Route exact path="/" component={ArtistList}/>
+                      <Route path="/artist/:id" component={Artist}/>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/ArtistList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ArtistList extends Component {
+    render() {
+        return (
+        <div>
+            <h2>These are the artists!</h2>
+        </div>
+        );
+    }
+}

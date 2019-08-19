86af38fc7f0502846f22243e74555c2939e95f41
Austin Blaine <theaustinblaine@gmail.com>
2019-08-19 14:25:20 -0400

    merch list is rendering with a better layout

--- client/src/components/MerchandiseList.jsx
+++ client/src/components/MerchandiseList.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 export default class MerchandiseList extends Component {
     state = {
@@ -35,10 +42,22 @@ export default class MerchandiseList extends Component {
         return (
         <div>
             <h3>Merchandise:</h3>
-            <div>
+            <div className="merch-list">
                 {artistMerchandise.map(merch => (
                     <Link to={`/artist/${this.props.artist.id}/merchandise/${merch.id}`}>
-                        <img src={merch.photo_url} alt={merch.name} width="150" height="" />
+                    
+                    <Card className="merch-card">
+                        <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            alt={merch.name}
+                            height="140"
+                            image={merch.photo_url}
+                            title="Contemplative Reptile"
+                        />
+                        </CardActionArea>
+                    </Card>
+
                     </Link>
                 ))}
             </div>
